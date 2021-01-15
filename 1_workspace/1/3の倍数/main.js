90acd06223e0a266a02a297abc566eb72072db50
konnnnb <chorusaku.t@gmail.com>
2021-01-15 15:13:22 +0900

    3倍数fizzbuzz3好き

---
+++ 1_workspace/1/3の倍数/main.js
@@ -0,0 +1,34 @@
+//3が大好き
+// const genkiFunction = function(number){
+//   for(let n=1; n<=number; n++){
+//   let nn=String(n)
+//   if (n % 3 === 0) {//同値は=三つ
+//     console.log(n + "!!!!!!!")
+//   } else if(nn.includes("3")===true){
+//     console.log(n+"!!!!!!!!")
+//   }else{
+//   console.log(n)
+//   }
+//   }
+//   }
+
+// genkiFunction(45)
+
+
+
+
+// //Fizzbazz
+// const genkiFunction = function(number){
+// for(let n=1; n<=number; n++){
+// if (n % 15 === 0) {//同値は=三つ
+//   console.log("FizzBuzz")
+// } else if(n % 5 === 0){
+//   console.log("Buzz")
+// }else if(n % 3 === 0){
+//   console.log("Fizz")
+// }else{
+//   console.log(n)
+// }
+// }
+// }
+// genkiFunction(45)
