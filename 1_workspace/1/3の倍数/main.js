c5744236b3ec79832472aef553e68457eec60f5b
konnnnb <chorusaku.t@gmail.com>
2021-01-19 09:33:44 +0900

    元気秒

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,18 +1,70 @@
-//3が大好き
+const display = document.getElementById("display")
+const button = document.getElementById("button")
+
+
+let count = 0
+
+const countUp = function(){
+    // count を更新
+    count += 1
+    // count を秒単位にして表示
+    if(count % 3 === 0){
+    display.textContent = count+"!!!!"
+    console.log(count + "!!!!")
+    }else{
+      display.textContent = count
+      console.log(count)
+    }
+}
+
+
+//数え上げ３元気
 const genkiFunction = function(number){
-  for(let n=1; n<=number; n++){
-  let nn=String(n)
+  setInterval(countUp, 1000)
+   for(let n=1; n<=number; n++){
   if (n % 3 === 0) {//同値は=三つ
     console.log(n + "!!!!!!!")
-  } else if(nn.includes("3")===true){
-    console.log(n+"!!!!!!!!")
-  }else{
-  console.log(n)
+  } else {
+    console.log(n)
   }
   }
   }
+  genkiFunction(24)
 
-genkiFunction(45)
+
+
+// let count = 0
+
+// const countUp = function(){
+//     // count を更新
+//     count += 1
+//     // count を秒単位にして表示
+//     display.textContent = count
+// }
+
+// button.onclick = function(){
+//     // 10ms ごとに countUp を実行するように登録する
+//     setInterval(countUp, 1000)
+//     button.textContent = "stop"
+// }
+
+
+
+// //3が大好き
+// const genkiFunction = function(number){
+//   for(let n=1; n<=number; n++){
+//   let nn=String(n)
+//   if (n % 3 === 0) {//同値は=三つ
+//     console.log(n + "!!!!!!!")
+//   } else if(nn.includes("3")===true){
+//     console.log(n+"!!!!!!!!")
+//   }else{
+//   console.log(n)
+//   }
+//   }
+//   }
+
+// genkiFunction(45)
 
 
 // //Fizzbazz
@@ -32,14 +84,15 @@ genkiFunction(45)
 // genkiFunction(45)
 
 
-// //3倍数
+//3倍数
 // const genkiFunction = function(number){
-//   for(let n=1; n<=number; n++){
-//   if (n % 3 === 0) {//同値は=三つ
-//     console.log(n + "!!!!!!!")
+//   setInterval(countUp, 1000)
+//    for(let n=1; n<=number; n++){
+//   if (count % 3 === 0) {//同値は=三つ
+//     console.log(count + "!!!!!!!")
 //   } else {
-//     console.log(n)
+//     console.log(count)
 //   }
 //   }
 //   }
-//   genkiFunction(45)
+//   genkiFunction(24)
