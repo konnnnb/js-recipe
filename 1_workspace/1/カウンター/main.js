20f5b976a5769a73a2c4972228d4adef67448d8d
konnnnb <chorusaku.t@gmail.com>
2021-01-22 10:25:04 +0900

    電卓

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,22 +1,89 @@
 const display = document.getElementById("display")
-const Button = document.getElementById("button")
+const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const multButton = document.getElementById("mult-button")
+const divButton = document.getElementById("div-button")
+const clearButton = document.getElementById("clear-button")
+const equalButton = document.getElementById("button")
+const inputform= document.getElementById("input-form")
+//let storage = "";//文字
+let result = 0;
+let enzan="=";
+//let count2 = "";
 
 
-let count=0//初期
-
-// ボタンをクリックしたときの処理を登録
-button.onclick = function(){
-  // ここにクリック後の処理を書く
-  count+=1
-
-  display.classList.add('red')
-
-  //count表示
-  display.textContent=count
+//+
+plusButton.onclick = function(){
+  calc();
+  enzan = "+"
 }
-window.onload = function (e) {
-  console.dir(e)
+
+//-
+minusButton.onclick = function(){
+  calc();
+  enzan = "-"
 }
+
+//*
+multButton.onclick = function(){
+  calc();
+  enzan = "*"
+}
+
+divButton.onclick = function(){
+  calc();
+  enzan = "/"
+}
+
+
+//clear
+clearButton.onclick = function(){
+ // storage=null
+  display.textContent=0
+}
+
+const calc=function(){
+
+  if(enzan === "+"){
+    result += Number(inputform.value)
+  }else if(enzan === "-"){
+    result -= Number(inputform.value)
+  }else if(enzan === "*"){
+    result *= Number(inputform.value)
+  }else if(enzan === "/"){
+    result /= Number(inputform.value)
+  }else if(enzan === "="){
+    result = Number(inputform.value)
+  }
+
+  display.textContent = result
+  inputform.value = ""
+}
+
+
+
+
+
+
+// const display = document.getElementById("display")
+// const Button = document.getElementById("button")
+
+
+// let count=0//初期
+
+// // ボタンをクリックしたときの処理を登録
+// button.onclick = function(){
+//   // ここにクリック後の処理を書く
+//   count+=1
+
+//   display.classList.add('red')
+
+//   //count表示
+//   display.textContent=count
+// }
+// window.onload = function (e) {
+//   console.dir(e)
+// }
 
 
 
